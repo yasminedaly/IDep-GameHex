d2e60e4a46796f59ee6a79f8738902e2c49098c7
amine <amineselmi99@gmail.com>
2022-04-03 23:11:22 +0100

    project ready to use

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/index2", name="index2")
+     */
+    public function index2(): Response
+    {
+        return $this->render('home/index2.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
